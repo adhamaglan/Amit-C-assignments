7fd1ea5d4d085fdf6b3eb1dcc529da5f36a77dbb
Adham <adhamaglan533@gmail.com>
2026-08-17 17:01:17 +0300

    adding preprocessor directives to std_types.h

--- std types assignment/std_types.h
+++ std types assignment/std_types.h
@@ -1,4 +1,7 @@
-//size in bits
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+
 typedef unsigned char             u8;
 typedef unsigned short           u16;
 typedef unsigned int             u32;
@@ -11,3 +14,5 @@ typedef signed long long         s64;
 
 typedef float                    f32;
 typedef double                   f64;
+
+#endif 
